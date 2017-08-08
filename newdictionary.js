df25a08c136994576a3d7e0dae4837b60c2c2bd0
kbourne87 <kpbourne87@gmail.com>
2017-08-08 18:19:03 -0400

    First Page for OverExplainer
    Word Finder, Definer

---
+++ newdictionary.js
@@ -0,0 +1,14 @@
+﻿var newDictionary = [
+	{word: "erudite", defOne: "having or showing great knowledge", defTwo: "scholarly"},
+	{word: "grandiloquent", defOne: "extravagant in language or style, especially with intent to impress", defTwo: "pomopus"},
+	{word: "quixotic", defOne: "exceedingly idealistic", defTwo: "unrealistic and impractical"},
+	{word: "MERCURIAL", defOne: "Subject to sudden or unpredictable changes of mood or mind", defTwo: "of or containing the element mercury"},
+	{word: "butt", defOne: "booty"},
+	{word: "proscribe", defOne: "forbid, especially by law", defTwo: "denounce or condemn"},
+	{word: "esoteric", defOne: "likely to be understood only by a small number of people", defTwo: "obscure"}
+
+
+
+
+
+];
